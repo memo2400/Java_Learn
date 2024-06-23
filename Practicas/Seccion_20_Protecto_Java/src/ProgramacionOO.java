3c37fc786441dccd35ece61ae2726d154f1530c0
desarrollador <guillermorosas00@gmail.com>
2024-06-23 16:20:11 -0600

    se termina seccion 20

--- Practicas/Seccion_20_Protecto_Java/src/ProgramacionOO.java
+++ Practicas/Seccion_20_Protecto_Java/src/ProgramacionOO.java
@@ -11,20 +11,52 @@ public class ProgramacionOO {
         // agregamos los datos de la persona
         persona1.nombre = "Pedro";
         persona1.apellido = "Perez";
-        persona1.edad = 40;
+        persona1.edad = 45;
+
+        // new instancia de la otra clase
+        Carrera carrera1 = new Carrera();
+        carrera1.nombre = "Ingenieria FHD";
+        carrera1.duracion = 6;
+        carrera1.EstaEnCurso = false;
+
+        // aqui se usa la clase carrera para meterla en la clase persona
+        persona1.carrera = carrera1;
+
 
         // imprimo
-        System.out.println(persona1.apellido + persona1.nombre + " tiene " + persona1.edad);
+        System.out.println(persona1.apellido + persona1.nombre + " tiene " + persona1.edad + " Esta recibido de " + persona1.carrera.nombre);
 
+        
+        
+        
         Persona persona2 = new Persona();
 
         persona2.apellido = "Juarez";
         persona2.nombre = "Benito";
         persona2.edad = 35;
 
-        System.out.println(persona2.apellido + persona2.nombre + " tiene " + persona2.edad);
+        
+        // new instancia de la otra clase
+        Carrera carrera2 = new Carrera();
+        carrera2.nombre = "Ingenieria";
+        carrera2.duracion = 6;
+        carrera2.EstaEnCurso = true;
+
+        persona2.carrera = carrera2;
+
+        System.out.println(persona2.apellido + persona2.nombre + " tiene " + persona2.edad + " Esta recibido de " + persona2.carrera.nombre);
         
         // 03:40:18
+
+
+        //ahora podemos hacer esto
+        System.out.println(persona1.DarNombreCompleto());
+        
+        System.out.println(persona1.EnviarSaludo("Luke Skywoker!!"));
+        System.out.println(persona2.EnviarSaludo("Luke Skywoker!!"));
+
+
+        // FIN  03:51:57
 
     }
 }
