7d39dc81d9184d1451c21f149d7d6494fb902bbe
desarrollador <guillermorosas00@gmail.com>
2024-06-25 19:22:04 -0600

    continuo 23

--- Practicas/Seccion_23_Encapsulamiento/src/Persona.java
+++ Practicas/Seccion_23_Encapsulamiento/src/Persona.java
@@ -5,12 +5,12 @@ public class Persona {
 
      /*
       * Atributos
-      *       
+      * Si estan Private no se puede acceder a ellos desde fuera      
       */
 
-     String nombre;
-     String apellido;
-     int edad;
+     private String nombre;
+     private String apellido;
+     private int edad;
      
 
     /*
@@ -18,48 +18,69 @@ public class Persona {
     * 
     */
 
-    public Persona GammaRay (int dato){
-
-    }
-
     // 04:11:40
-
-    public Persona (String NombreParametro, String apellido, int edad, String NombreCarrera , int DuracionCarrera, boolean EstaCursando){
-
-        // aqui genero la instancia para que en el main ya no deba hacer la instancia.
-        carrera = new Carrera(NombreCarrera, DuracionCarrera, EstaCursando);
-
-        // this.nombre = es de los atributos  despues del "=" es del constructor de aqui arriba osea el parametro de la funcion- constructor        
+    // Constructor
+    public Persona (String NombreParametro, String apellido, int edad){
         
         this.nombre = NombreParametro;
         this.apellido = apellido;
         this.edad = edad;
-
-        /* Lo comente debido a que ya no se usara por ahora asi, el constructor hace todo
-        // aqui gacenmos el accero a la clase carrera que es del otro archivo, como el parametro es distinto no ponemos el this
-        carrera.nombre = NombreCarrera;
-        */ 
+        
     }
 
+    // Metodos o "funciones"
     public String DarNombreCompleto(){
-
-        // se usa para regresar algo, como vemos se hace directo
+        
         return apellido + " " + nombre;
     }
 
-
-    // Aremos una con parametros
+    
     public String EnviarSaludo (String saludado){
         
-        // si es mayor a 40, se corta el reto del metodo, osea solo lelgo hasta el if
-        // se usa solo una linea de codigo, sin llaves {}
-
+        
         if (edad > 40) return "Muy buen dia Señor " + saludado + " ";
         return "hola como estas " + saludado + "!!!";
+    }
 
-        
-        // aqui abajo ya no pondriamos nada por que el retunr corta
+    /*
+     * Se bucsca el metodo get para acceder a la edad privada desde fuera.
+     * ES DE TIPO Int porque retorna cosas
+     */
+
+
+    public int getEdad() {
+
+        return edad;
 
     }
+
+    /*
+     * Devuelve nombre en Mayuculas NOMBRE
+     */
+
+    public String getNombre() {
+
+        return nombre.toUpperCase();
+    }
+     
+    /*
+     * Como este no retorna nada sera void
+     * Es para setear
+     */
+
+    public void setNombre(String NombreParametro ){
+
+        this.nombre = NombreParametro;
+
+    }
+
+    public void setApellido (String ApellidoParametro){
+
+        this.apellido = ApellidoParametro;
+
+    }
+
+    // 04:19:00
+
 
 }
--- Practicas/Seccion_23_Encapsulamiento/src/ProgramacionOO.java
+++ Practicas/Seccion_23_Encapsulamiento/src/ProgramacionOO.java
@@ -1,60 +1,34 @@
 // Esta es nuectra clase
 public class ProgramacionOO {
     
-    // Este es nuestro metodo, de aqui el main nace todo
-    // osea otras clases deben ser llamadas desde aqui
+    
     public static void main(String[] args) throws Exception {
         
-        // new es para instancias
-        Persona persona1 = new Persona("Xi", "Xin Ping", 60, "Lider Supremo", 20, false);
-        // voy a ctrl + , = para usar inlay hints, que nos sive de ayuda
-        // 04:01:28 
-        System.out.println(persona1.apellido + persona1.nombre + " tiene " + persona1.edad + " Esta recibido de " + persona1.carrera.nombre);
-
-        /* // Coemtop esto al ahora tener el constructor FHD
-        persona1.nombre = "Xi";
-        persona1.apellido = "Jin Ping";
-        persona1.edad = 55;
-        */
-        //persona1.carrera.nombre ="Lider"; 
-        // esto aunq ue no marca error no funcionaria ya que de forma previa debemos crear la instancia de carrera
-
-        // ahora que hicimos instancia dentro de clase persona ya podemos de forma directa
-        persona1.carrera.nombre ="segunda carrera"; 
-         
-
+        // Si es private solo podemos acceder a los nombre apelido una vez cuando se hace instancia.
+        Persona persona1 = new Persona("Xi", "Xin Ping", 60);
         
-
-
         /*
-        // new instancia de la otra clase
-        Carrera carrera1 = new Carrera();
-        
-        carrera1.nombre = "Ingenieria FHD";
-        carrera1.duracion = 6;
-        carrera1.EstaEnCurso = false;
-
-        
-        // aqui se usa la clase carrera para meterla en la clase persona
-        persona1.carrera = carrera1;
-
-        */
+         * Para aaceder adecuadamente a lo 
+         * que esta privado usamos "persona1.getEdad()
+         */
+        //System.out.println(persona1.apellido + persona1.nombre + " tiene " + persona1.getEdad());
 
 
-        // imprimo
-        System.out.println(persona1.apellido + persona1.nombre + " tiene " + persona1.edad + " Esta recibido de " + persona1.carrera.nombre);
+         /*
+          * Para escribir nombre nuevamente
+          */
 
+        persona1.setApellido("Hansen");  
                 
         
 
 
         //ahora podemos hacer esto
-        System.out.println(persona1.DarNombreCompleto());
+        System.out.println(persona1.DarNombreCompleto() + " " + persona1.getEdad());
         
         System.out.println(persona1.EnviarSaludo("Luke Skywoker!!"));        
-
-        // 04:05:09 Fin
         
+        System.out.println("Mayuscula: " + persona1.getNombre());        
 
     }
 }
