5abc59d32a643c911ea85b26733d01f7d53cb9e5
Guillermo Rosas <memorosas.00@gmail.com>
2025-10-06 15:54:03 -0600

    se termina colecciones tipo **set

--- Practicas/Seccion_31_Handset_etc/src/App.java
+++ Practicas/Seccion_31_Handset_etc/src/App.java
@@ -1,5 +1,7 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -26,6 +28,59 @@ public class App {
         if (!superHeroes.contains("batman")){
             System.out.println("batman se ha ido");
         }
+
+        System.out.println(superHeroes);
+
+
+        // X2 no se agregan los repetidos
+        superHeroes.add("superman");
+        superHeroes.add("superman");
+        superHeroes.add("superman");
+
+        if (superHeroes.isEmpty()){
+            System.out.println("La fiesta acabo");
+        }
+        else{
+            System.out.println("La fiesta aun no acaba, hay: "+ superHeroes.size() + " superheroes" );
+        }
+
+        System.out.println("Aun estan en la fiesta:");
+        // For each pero la lista esta desornedana
+        for (String superheroeIndividual : superHeroes){
+            System.out.println(superheroeIndividual);
+        }
+
+
+        /// ###############################//#endregion
+
+        // TREESET ordena alfabeticamente por default
+        Set<String> carros = new TreeSet<>();
+
+        carros.add("coche");
+        carros.add("camion");
+        carros.add("bocho");
+
+        System.out.println("\n  Los carros son:");
+        
+        for (String carro : carros){
+            System.out.println(carro);
+        }
+
+                /// ###############################//#endregion
+
+        // LINKED ordena como los agregamos
+        Set<String> vehiculos = new LinkedHashSet<>();
+
+        vehiculos.add("coche");
+        vehiculos.add("camion");
+        vehiculos.add("bocho");
+
+        System.out.println("\n  Los carros son (Link):");
+        
+        for (String carro : vehiculos){
+            System.out.println(carro);
+        }
+
 
     }
 }
