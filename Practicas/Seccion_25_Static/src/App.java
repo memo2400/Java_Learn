8b65ec39a7fe7d551ed3da4c5c3bb4dc010eab5c
Guillermo Rosas <memorosas.00@gmail.com>
2025-10-13 10:43:31 -0600

    corrije falta ortografica

--- Practicas/Seccion_25_Static/src/App.java
+++ Practicas/Seccion_25_Static/src/App.java
@@ -2,14 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         /*
-         * palabra Sttic hace que sea metodo de clase o atributo de clase
+         * palabra Static hace que sea metodo de clase o atributo de clase
          * esta no se necvesitara instanciar, ya que estara en todas las
          * partes del proyecto
          * 
          * 
          */
         Animal anim = new Animal("elefante", 20);
-        Gato gatito = new Gato("negor", 5);
+        Gato gatito = new Gato("negro", 5);
 
 
 
